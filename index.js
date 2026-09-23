aea17d7ada9c4a851939c8d596c98fb46c6089aa
esjim01 <eduar.jimenez45164@ucaldas.edu.co>
2026-09-23 18:57:03 -0500

    Update: prueba de despliege 23-09-2026

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const environment = process.env.APP_ENVIRONMENT || "LOCAL";
 
 app.get('/api/hello', (req, res) => {
   res.json({
-    message: "Hola desde DevOps",
+    message: "Hola desde DevOps, despliegue de prueba 23-09-2026",
     environment: environment
   });
 });
+
+app.post
 
 // Solo levanta el servidor si este archivo se ejecuta directamente,
 // no cuando lo importa el test
