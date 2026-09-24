76787c7a1f9165dc6dbd250cd1bf4d3769ecb720
esjim01 <eduar.jimenez45164@ucaldas.edu.co>
2026-09-23 19:13:08 -0500

    Update: prueba de despliege 23-09-2026

--- test/hello.test.js
+++ test/hello.test.js
@@ -5,7 +5,7 @@ describe('GET /api/hello', () => {
   it('responde con status 200 y el mensaje esperado', async () => {
     const res = await request(app).get('/api/hello');
     expect(res.statusCode).toBe(200);
-    expect(res.body.message).toBe("Hola desde DevOps");
+    expect(res.body.message).toBe("Hola desde DevOps, despliegue de prueba 23-09-2026");
     expect(res.body).toHaveProperty('environment');
   });
 });
